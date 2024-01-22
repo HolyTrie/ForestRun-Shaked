f3effe8cd63065b750c5ff8e38b8206d63c4e639
20shaked20 <shakedlevi32@gmail.com>
2024-01-22 21:49:32 +0200

    fixed issue with camera

--- Assets/Scripts/MoveCameraVertical.cs
+++ Assets/Scripts/MoveCameraVertical.cs
@@ -8,7 +8,7 @@ public class MoveCameraVertical : MonoBehaviour
     private void Update()
     {
         transform.position = new Vector3(CameraFollow.transform.position.x, 0f, CameraFollow.transform.position.z) + offset;
-        if (CameraFollow.transform.position.y < 0)
+        if (CameraFollow.transform.position.y > 0)
         {
             transform.position = new Vector3(CameraFollow.transform.position.x, CameraFollow.transform.position.y, CameraFollow.transform.position.z) + offset;
         }
