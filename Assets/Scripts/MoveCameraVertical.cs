037e6b6bde8b51c2f489b09a9f90757eb15c4fc6
20shaked20 <shakedlevi32@gmail.com>
2024-01-22 20:40:42 +0200

    camera follow is now good!

---
+++ Assets/Scripts/MoveCameraVertical.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCameraVertical : MonoBehaviour
+{
+    public GameObject CameraFollow;
+    public Vector3 offset;
+    
+    private void Update()
+    {
+        transform.position = new Vector3(CameraFollow.transform.position.x, 0f, CameraFollow.transform.position.z) + offset;
+        if (CameraFollow.transform.position.y < 0)
+        {
+            transform.position = new Vector3(CameraFollow.transform.position.x, CameraFollow.transform.position.y, CameraFollow.transform.position.z) + offset;
+        }
+    }
+}
